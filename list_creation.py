7be9c381c65f7b0ac061cbb502647bf4a0bf422b
Muthu Ramasamy <40240197+muthuramasamy@users.noreply.github.com>
2019-06-20 10:29:32 +0530

    Create list_creation.py

---
+++ list_creation.py
@@ -0,0 +1,9 @@
+list_of_airlines=["AI","EM","BA"]
+
+print("Iterating the list using range()")
+for index in range(0,len(list_of_airlines)):
+    print(list_of_airlines[index])
+
+print("Iterating the list using keyword in")
+for airline in list_of_airlines:
+    print(airline)
